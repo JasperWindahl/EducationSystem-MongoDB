17922f584308f9236b67913a550971a88fdde937
Jasper Windahl Andersen <jwin74@users.noreply.github.com>
2021-05-26 20:54:04 +0200

    Updated Authentication Models

--- WebApi/Authentication/TokenDetails.cs
+++ WebApi/Authentication/TokenDetails.cs
@@ -7,5 +7,8 @@ namespace WebApi.Authentication
 {
     public class TokenDetails
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Roles { get; set; }
     }
 }
--- WebApi/Authentication/TokenRequest.cs
+++ WebApi/Authentication/TokenRequest.cs
@@ -7,5 +7,7 @@ namespace WebApi.Authentication
 {
     public class TokenRequest
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
