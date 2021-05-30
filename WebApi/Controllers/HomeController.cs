cb1c67e13e81130b456025d4e01b77d7b0e1f474
Jasper Windahl Andersen <jwin74@users.noreply.github.com>
2021-05-30 22:59:07 +0200

    Added Home Controller

---
+++ WebApi/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHome()
+        {
+            return Ok("Service Ready!, Access service thru /api");
+        }
+        [Route("api")]
+        [HttpGet]
+        public IActionResult GetApi()
+        {
+            return Ok("Service requires Authentication, please login before proceeding!");
+        }
+    }
+}
